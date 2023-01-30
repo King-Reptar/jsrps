8b7da784a4d8ecddbbe4b4296630bc5ca5085495
King-Reptar <103396194+King-Reptar@users.noreply.github.com>
2023-01-29 17:18:48 -0700

    Player and Computer choice displaying

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+
+let move = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice(move) {
 
     return move[Math.floor(Math.random() * move.length)];
 }
-let move = ["Rock", "Paper", "Scissors"];
-console.log(getComputerChoice(move));
+//Working so far..
+
+let playerSelection = "Rock";
+let computerSelection = getComputerChoice(move);
+
+function playRound(playerSelection, computerSelection) {
+    return (playerSelection + computerSelection);
+}
+console.log(playRound(playerSelection, computerSelection));
+
