3b8ae39cc75327d0a673415b8c6ee467e6b2ad95
King-Reptar <103396194+King-Reptar@users.noreply.github.com>
2023-01-30 22:09:11 -0700

    It's ALIVE
    Added a user input a set all the if/else statements.  It's finally
    coming together.  Next up is retaining the result of each round.

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 
-let move = ["Rock", "Paper", "Scissors"];
+let move = ["rock", "paper", "scissors"];
 
 function getComputerChoice(move) {
 
     return move[Math.floor(Math.random() * move.length)];
 }
-//Working so far..
 
-let playerSelection = "Rock";
+let playerSelection = prompt("Your Move");
+
 let computerSelection = getComputerChoice(move);
 
+//Function to return the computer's choice
 function playRound(playerSelection, computerSelection) {
     return (playerSelection + computerSelection);
 }
-console.log(playRound(playerSelection, computerSelection));
-
+ 
+//If Else statements declaring the outcome of the round
+if (playerSelection.toLowerCase() == computerSelection) {
+    console.log(`It's a Tie!!`)
+}
+else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+    console.log("Paper beats rock... You Lose!")
+}
+else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+    console.log("Rock beats Scissors... YOU WIN!!")
+}
+else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+    console.log("Scissors beats Paper... YOU WIN!!")
+}
+else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+    console.log("Rock beats Scissors... You Lose!")
+}
+else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+    console.log("Scissors beat Paper... You Lose")
+}
+else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+    console.log("Paper beats Rock... YOU WIN!!")
+}
